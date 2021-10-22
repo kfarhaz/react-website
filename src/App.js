093b02108885661d9b6af27869bb91b2c90b6141
Farhaz Khan <farhaz.khan@GIPL.INET>
2021-10-22 18:43:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Navbar, Footer} from './components';
+import Home from './pages/HomePage/Home';
+
+
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,63 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goal.',
+    headline: 'Lead Generation Specialist for Online Business',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require("../../images/svg-2.svg").default,
+    alt: 'Image',
+    start: ''
+};
+
+export const homeObjTwo = {
+    primary: false,
+    lightBg: true,
+    imgStart: 'start',
+    lightTopLine: false,
+    lightTextDesc: false,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goal.',
+    headline: 'Lead Generation Specialist for Online Business',
+    lightText: false,
+    topLine: 'Marketing Agency',
+    img: require("../../images/profile.jpg").default,
+    alt: 'Image',
+    start: 'true'
+};
+
+export const homeObjThree = {
+    primary: true,
+    lightBg: false,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goal.',
+    headline: 'Lead Generation Specialist for Online Business',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require("../../images/svg-1.svg").default,
+    alt: 'Image',
+    start: ''
+};
+
+export const homeObjFour = {
+    primary: true,
+    lightBg: false,
+    imgStart: 'start',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goal.',
+    headline: 'Lead Generation Specialist for Online Business',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require("../../images/svg-3.svg").default,
+    alt: 'Image',
+    start: 'true'
+};
